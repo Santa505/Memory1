f5ae4741a0fd43a4f5c6dd67d38411bc44cfbfbc
jeff indall <jeffindall@gmail.com>
2018-05-29 10:22:27 -0600

    final commit

--- seeds/01_todo.js
+++ seeds/01_todo.js
@@ -1,16 +1,8 @@
 const todo = require('../todo');
 
-// exports.seed = function(knex, Promise) {
-//   return knex('todo').del()
-//     .then(function () {
-//       return knex('todo').insert(todo);
-//     });
-// };
 exports.seed = function (knex, Promise) {
- // Deletes ALL existing entries
- return knex.raw('DELETE FROM "todo"; ALTER SEQUENCE todo_id_seq RESTART WITH 4; ')
+  return knex.raw('DELETE FROM "todo"; ALTER SEQUENCE todo_id_seq RESTART WITH 4; ')
   .then(function () {
-   // Inserts seed entries
-   return knex('todo').insert(todo)
+  return knex('todo').insert(todo)
  });
 };
